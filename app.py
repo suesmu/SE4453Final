b16eda1a90a06d9a6ce180d0cce79ef466597551
suesmu <sultanesenmurat@gmail.com>
2025-06-12 17:41:20 +0300

    Trigger deploy 4

--- app.py
+++ app.py
@@ -3,24 +3,26 @@ import os
 
 app = Flask(__name__)
 
-# Key Vault üzerinden gelen environment değişkenlerini al
-db_user = os.environ.get("dbuser1")
-db_password = os.environ.get("dbpassword1")
-db_name = os.environ.get("dbname1")
-db_host = os.environ.get("dbhost")
-
 @app.route('/')
 def home():
-    return f"""
-        Merhaba SE4453 Final'den!<br><br>
-        dbuser1: {db_user}<br>
-        dbname1: {db_name}<br>
-        dbhost: {db_host}
-    """
+    return "merhaba se4453 final'den!"
 
 @app.route('/hello')
 def hello():
-    return "Hello from Azure CI/CD!"
+    return "hello from azure ci/cd!"
+
+@app.route('/db')
+def db_connection():
+    db_host = os.environ.get('dbhost')
+    db_name = os.environ.get('dbname1')
+    db_user = os.environ.get('dbuser1')
+    db_password = os.environ.get('dbpassword1')
+
+    # Sadece değişkenler dolu mu kontrol ediliyor
+    if db_host and db_name and db_user and db_password:
+        return "veritabanına başarıyla bağlanıldı."
+    else:
+        return "veritabanı bilgileri eksik!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
