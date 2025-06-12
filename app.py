2fa102c837ceec8ca32a7e2b7857602822abfe13
suesmu <sultanesenmurat@gmail.com>
2025-06-12 17:15:04 +0300

    Trigger deploy

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import os
 
 app = Flask(__name__)
 
+# Veritabanı bağlantı bilgilerini environment variables üzerinden al
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+
 @app.route('/')
 def home():
-    return "Merhaba SE4453 Final'den!"
+    return f"Merhaba SE4453 Final'den!<br><br>" \
+           f"DB_USER: {DB_USER}<br>" \
+           f"DB_NAME: {DB_NAME}<br>" \
+           f"DB_HOST: {DB_HOST}"
 
 @app.route('/hello')
 def hello():
